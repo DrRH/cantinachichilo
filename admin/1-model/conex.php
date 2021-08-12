04826edfed7c09f136510642d2cd1accc3e4a00c
DrRH <64128983+DrRH@users.noreply.github.com>
2021-08-12 11:53:52 -0500

    Update conex.php

--- admin/1-model/conex.php
+++ admin/1-model/conex.php
@@ -1,7 +1,7 @@
 <?php
 echo 'esta es la página de conexión a la BD';
 #Parámetros de conexión a la base de datos del aplicativo
-$host="http://62.171.160.194/localhost";
+$host="62.171.160.194";
 $port=3306;
 $socket="";
 $user="Dorian_admin";
